9788bddca59f332709ffb1370a37a8e5eb58b515
zsp19931222 <872126510@qq.com>
2020-03-26 16:25:18 +0800

    初识Android官方架构组件Navigation

---
+++ app/src/main/java/com/zsp/myjetpackdemo/BlankFragment.kt
@@ -0,0 +1,39 @@
+package com.zsp.myjetpackdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_blank.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn2.setOnClickListener {
+            Navigation.findNavController(it)
+                .navigate(R.id.action_blankFragment_to_blankFragment2);
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank, container, false)
+    }
+}
---
+++ app/src/main/java/com/zsp/myjetpackdemo/BlankFragment2.kt
@@ -0,0 +1,40 @@
+package com.zsp.myjetpackdemo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_blank2.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment2 : Fragment() {
+    // TODO: Rename and change types of parameters
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn.setOnClickListener{
+            Navigation.findNavController(it).navigate(R.id.action_blankFragment2_to_blankFragment3)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank2, container, false)
+    }
+
+}
