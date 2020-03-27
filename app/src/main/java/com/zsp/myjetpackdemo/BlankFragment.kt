f9fee213fe24c1079c4d945bd51b8534adee187b
zsp19931222 <872126510@qq.com>
2020-03-27 10:35:15 +0800

    viewBinding视图绑定

--- app/src/main/java/com/zsp/myjetpackdemo/BlankFragment.kt
+++ app/src/main/java/com/zsp/myjetpackdemo/BlankFragment.kt
@@ -1,11 +1,14 @@
 package com.zsp.myjetpackdemo
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import com.zsp.myjetpackdemo.databinding.FragmentBlankBinding
 import kotlinx.android.synthetic.main.fragment_blank.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -19,11 +22,16 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BlankFragment : Fragment() {
+    //在Kotlin中定义变量或者属性都是需要初始化值的，并且其都是private的，但是有些时候对于变量或者属性只需要声明，但是不需要初始化，则kotlin提供了lateinit关键字来实现
+//    lateinit 对应使用var来声明属性
+//    lateinit 修饰不可以修饰原始数据类型（byte，char，short ,int，long，float，double）
+    private lateinit var binding: FragmentBlankBinding
     // TODO: Rename and change types of parameters
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        btn2.setOnClickListener {
-            Navigation.findNavController(it)
+        binding.btn2.setOnClickListener {
+            Toast.makeText(activity,"1111111",Toast.LENGTH_SHORT).show()
+            Navigation.findNavController(binding.btn2)
                 .navigate(R.id.action_blankFragment_to_blankFragment2);
         }
     }
@@ -33,7 +41,8 @@ class BlankFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding=FragmentBlankBinding.inflate(inflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false)
+        return binding.root
     }
 }
